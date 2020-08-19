19bca92d1cc764e79425d75446c66c7473e94ab5
ashu12chi <ashutosh7234@gmail.com>
2020-08-19 17:01:42 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/inherited_widgets/note_inherited_widget.dart';
+import 'package:splashscreen/splashscreen.dart';
 import './app_screens/note_list.dart';
 
 
-void main() => runApp(App());
+void main() {
+  runApp(
+    new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new _SplashScreen(),
+    )
+  );
+}
+
+class _SplashScreen extends StatefulWidget {
+  @override
+  _SplashState createState() => new _SplashState();
+}
+
+class _SplashState extends State<_SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: App(),
+      image: new Image.asset('assets/icon/icon.png'),
+      title: new Text(
+        '\t\t\t\t\t\t\t\t\t\tNoteApp\nAshutosh Chitranshi',
+        style: new TextStyle(fontSize: 25.0),
+      ),
+      backgroundColor: Colors.white,
+      photoSize: 100.0,
+      loaderColor: Colors.white,
+    );
+  }
+
+}
 
 class App extends StatelessWidget {
   @override
