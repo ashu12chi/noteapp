81740b616e88392b2063ae6cb1b115db043d5d39
ashu12chi <ashutosh7234@gmail.com>
2020-08-19 18:35:23 +0530

    fingerprint authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/auth_strings.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:noteapp/inherited_widgets/note_inherited_widget.dart';
 import 'package:splashscreen/splashscreen.dart';
 import './app_screens/note_list.dart';
@@ -6,7 +8,7 @@ import './app_screens/note_list.dart';
 
 void main() {
   runApp(
-    new MaterialApp(
+    new MaterialApp (
       debugShowCheckedModeBanner: false,
       home: new _SplashScreen(),
     )
@@ -23,7 +25,7 @@ class _SplashState extends State<_SplashScreen> {
   Widget build(BuildContext context) {
     return new SplashScreen(
       seconds: 5,
-      navigateAfterSeconds: App(),
+      navigateAfterSeconds: LocalAuth(),
       image: new Image.asset('assets/icon/icon.png'),
       title: new Text(
         '\t\t\t\t\t\t\t\t\t\tNoteApp\nAshutosh Chitranshi',
@@ -33,6 +35,96 @@ class _SplashState extends State<_SplashScreen> {
       photoSize: 100.0,
       loaderColor: Colors.white,
     );
+  }
+
+}
+
+class LocalAuth extends StatefulWidget {
+  @override
+  _LocalAuthState createState() => _LocalAuthState();
+}
+
+class _LocalAuthState extends State<LocalAuth> {
+  bool isAuth = false;
+  @override
+  Widget build(BuildContext context) {
+   return isAuth?App():Scaffold(
+     backgroundColor: Colors.white,
+     body: Center(
+       child: InkWell(
+         onTap: () {
+           _checkBiometric();
+         },
+         child: Container(
+           height: 60,
+           width: MediaQuery.of(context).size.width * 0.9,
+           decoration: BoxDecoration(
+           border: Border.all(color: Colors.blueAccent, width: 2.5)),
+           child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+           Icon(
+            Icons.fingerprint,
+            color: Colors.blueAccent,
+           ),
+           Text(
+            "Login with Fingerprint",
+            style: TextStyle(color: Colors.blueAccent),
+           )
+         ],
+         ),
+       ),
+     ),
+   )
+   );
+  }
+
+  void _checkBiometric() async {
+    // check for biometric availability
+    final LocalAuthentication auth = LocalAuthentication();
+    bool canCheckBiometrics = false;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+    } catch (e) {
+      print("error biometrics $e");
+    }
+
+    print("biometric is available: $canCheckBiometrics");
+
+    // enumerate biometric technologies
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await auth.getAvailableBiometrics();
+    } catch (e) {
+      print("error enumerate biometrics $e");
+    }
+
+    print("following biometrics are available");
+    if (availableBiometrics.isNotEmpty) {
+      availableBiometrics.forEach((ab) {
+        print("\ttech: $ab");
+      });
+    } else {
+      print("no biometrics are available");
+    }
+
+    // authenticate with biometrics
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticateWithBiometrics(
+          localizedReason: 'Touch your finger on the sensor to login',
+          useErrorDialogs: true,
+          stickyAuth: false,
+          androidAuthStrings:
+          AndroidAuthMessages(signInTitle: "Login to HomePage"));
+    } catch (e) {
+      print("error using biometric auth: $e");
+    }
+    setState(() {
+      isAuth = authenticated ? true : false;
+    });
+
+    print("authenticated: $authenticated");
   }
 
 }
